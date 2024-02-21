661ee89d9e653236f7e7f57e6027a8c7a35acfe0
Kaushik <kaushikjoshi827@gmail.com>
2024-02-21 21:32:16 +0530

    Fixed middleware issue

--- src/middleware.js
+++ src/middleware.js
@@ -1,28 +1,23 @@
 import { NextResponse } from "next/server";
 
-
-
-export function middleware(request){
-
-    // get cookie
-    let cookie = request.cookies.get("token");
-    console.log("called.." , request.url)
-    if(request.nextUrl.pathname.startsWith("/auth")){
-        console.log(cookie)
-        if(cookie?.value){
-            return NextResponse.redirect(new URL("/" , request.url))
-        }
-    }else{
-        if(!cookie?.value){
-            return NextResponse.redirect(new URL("/auth/login" , request.url))
-        }
+export function middleware(request) {
+  // get cookie
+  let cookie = request.cookies.get("token");
+  console.log("called..", request.url);
+  if (request.nextUrl.pathname.startsWith("/auth")) {
+    console.log(cookie);
+    if (cookie?.value) {
+      return NextResponse.redirect(new URL("/", request.url));
     }
+  } else {
+    if (!cookie?.value) {
+      return NextResponse.redirect(new URL("/auth/login", request.url));
+    }
+  }
 
-    return NextResponse.next();
+  return NextResponse.next();
 }
 
 export const config = {
-    matcher:['/auth/:path*' , '/categories','/']
-}
-
-
+  matcher: ["/auth/:path*", "/categories", "/"],
+};
